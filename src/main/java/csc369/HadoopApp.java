09bd9ccf566e2d12eb73858586894e0ba87f133b
hahuang2 <anthonyhg1818@gmail.com>
2025-10-17 22:41:51 -0700

    updated hadoop

--- src/main/java/csc369/HadoopApp.java
+++ src/main/java/csc369/HadoopApp.java
@@ -42,11 +42,57 @@ public class HadoopApp {
 	    job.setOutputValueClass(UserMessages.OUTPUT_VALUE_CLASS);
 	    FileOutputFormat.setOutputPath(job, new Path(otherArgs[3]));
 
-	} else if ("WordCount".equalsIgnoreCase(otherArgs[0])) {
-	    job.setReducerClass(WordCount.ReducerImpl.class);
-	    job.setMapperClass(WordCount.MapperImpl.class);
-	    job.setOutputKeyClass(WordCount.OUTPUT_KEY_CLASS);
-	    job.setOutputValueClass(WordCount.OUTPUT_VALUE_CLASS);
+	} else if ("HostCountryJoin".equalsIgnoreCase(otherArgs[0])) {
+
+	    MultipleInputs.addInputPath(job, new Path(otherArgs[1]),
+					KeyValueTextInputFormat.class, HostCountryJoin.CountryMapper.class );
+	    MultipleInputs.addInputPath(job, new Path(otherArgs[2]),
+					TextInputFormat.class, HostCountryJoin.LogMapper.class ); 
+
+	    job.setReducerClass(HostCountryJoin.JoinReducer.class);
+
+	    job.setOutputKeyClass(HostCountryJoin.OUTPUT_KEY_CLASS);
+	    job.setOutputValueClass(HostCountryJoin.OUTPUT_VALUE_CLASS);
+	    FileOutputFormat.setOutputPath(job, new Path(otherArgs[3]));
+
+	}else if ("HostCountryUrlJoin".equalsIgnoreCase(otherArgs[0])) {
+
+	    MultipleInputs.addInputPath(job, new Path(otherArgs[1]),
+					KeyValueTextInputFormat.class, HostCountryUrlJoin.CountryMapper.class );
+	    MultipleInputs.addInputPath(job, new Path(otherArgs[2]),
+					TextInputFormat.class, HostCountryUrlJoin.LogMapper.class ); 
+
+	    job.setReducerClass(HostCountryUrlJoin.JoinReducer.class);
+
+	    job.setOutputKeyClass(HostCountryUrlJoin.OUTPUT_KEY_CLASS);
+	    job.setOutputValueClass(HostCountryUrlJoin.OUTPUT_VALUE_CLASS);
+	    FileOutputFormat.setOutputPath(job, new Path(otherArgs[3]));
+
+	}else if ("UrlCountryList".equalsIgnoreCase(otherArgs[0])) {
+
+		MultipleInputs.addInputPath(job, new Path(otherArgs[1]),
+					KeyValueTextInputFormat.class, UrlCountryList.CountryMapper.class);
+		MultipleInputs.addInputPath(job, new Path(otherArgs[2]),
+					TextInputFormat.class, UrlCountryList.LogMapper.class);
+
+		job.setReducerClass(UrlCountryList.JoinReducer.class);
+
+		job.setOutputKeyClass(UrlCountryList.OUTPUT_KEY_CLASS);
+		job.setOutputValueClass(UrlCountryList.OUTPUT_VALUE_CLASS);
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[3]));
+	}	
+ 	else if ("Sum".equalsIgnoreCase(otherArgs[0])) {
+	    job.setReducerClass(Sum.ReducerImpl.class);
+	    job.setMapperClass(Sum.MapperImpl.class);
+	    job.setOutputKeyClass(Sum.OUTPUT_KEY_CLASS);
+	    job.setOutputValueClass(Sum.OUTPUT_VALUE_CLASS);
+	    FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
+	    FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
+	} else if ("sortDes".equalsIgnoreCase(otherArgs[0])) {
+	    job.setReducerClass(SortDes.ReducerImpl.class);
+	    job.setMapperClass(SortDes.MapperImpl.class);
+	    job.setOutputKeyClass(SortDes.OUTPUT_KEY_CLASS);
+	    job.setOutputValueClass(SortDes.OUTPUT_VALUE_CLASS);
 	    FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
 	    FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
 	} else if ("AccessLog".equalsIgnoreCase(otherArgs[0])) {
@@ -56,7 +102,35 @@ public class HadoopApp {
 	    job.setOutputValueClass(AccessLog.OUTPUT_VALUE_CLASS);
 	    FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
 	    FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
-	} else {
+	}else if ("CountryUrlCount".equalsIgnoreCase(otherArgs[0])) {
+	    job.setReducerClass(CountryUrlCount.ReducerImpl.class);
+	    job.setMapperClass(CountryUrlCount.MapperImpl.class);
+	    job.setOutputKeyClass(CountryUrlCount.OUTPUT_KEY_CLASS);
+	    job.setOutputValueClass(CountryUrlCount.OUTPUT_VALUE_CLASS);
+	    FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
+	    FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
+	}else if ("SumCountryUrl".equalsIgnoreCase(otherArgs[0])) {
+	    job.setReducerClass(SumCountryUrl.ReducerImpl.class);
+	    job.setMapperClass(SumCountryUrl.MapperImpl.class);
+	    job.setOutputKeyClass(SumCountryUrl.OUTPUT_KEY_CLASS);
+	    job.setOutputValueClass(SumCountryUrl.OUTPUT_VALUE_CLASS);
+	    FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
+	    FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
+	}else if ("SortCountryAZ".equalsIgnoreCase(otherArgs[0])) {
+	    job.setReducerClass(SortCountryAZ.ReducerImpl.class);
+	    job.setMapperClass(SortCountryAZ.MapperImpl.class);
+	    job.setOutputKeyClass(SortCountryAZ.OUTPUT_KEY_CLASS);
+	    job.setOutputValueClass(SortCountryAZ.OUTPUT_VALUE_CLASS);
+	    FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
+	    FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
+	}else if ("UrlCountryAgg".equalsIgnoreCase(otherArgs[0])) {
+	    job.setReducerClass(UrlCountryAgg.ReducerImpl.class);
+	    job.setMapperClass(UrlCountryAgg.MapperImpl.class);
+	    job.setOutputKeyClass(UrlCountryAgg.OUTPUT_KEY_CLASS);
+	    job.setOutputValueClass(UrlCountryAgg.OUTPUT_VALUE_CLASS);
+	    FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
+	    FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
+	}  else {
 	    System.out.println("Unrecognized job: " + otherArgs[0]);
 	    System.exit(-1);
 	}
